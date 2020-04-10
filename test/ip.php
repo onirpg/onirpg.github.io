b1ce39eae3bcef0f3768245f371d18c9cdb78d16
onirpg <63267841+onirpg@users.noreply.github.com>
2020-04-10 15:02:41 +0900

    Delete ip.php

--- test/ip.php
+++
@@ -1,8 +0,0 @@
-<?php
-//javascriptファイルとして処理させる
-header("Content-type: application/x-javascript");
-//IPアドレスを変数に代入
-$ip_address = $_SERVER['REMOTE_ADDR'];
-//スクリプトの出力
-echo "var ipAddress = '" . $ip_address . "';";
-?>
